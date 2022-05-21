f5553c04bfee60ac3f7ccf9701c3427f5cc93cae
Kevin cuadros <kevindavidcuadros@gmail.com>
2022-05-20 21:37:16 -0500

    sanity config

---
+++ studio/schemas/userScheme.js
@@ -0,0 +1,25 @@
+
+const userSchema = {
+  name: 'users',
+  title: 'Users',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string'
+    },
+    {
+      name: 'walletAddress',
+      title: 'Wallet Address',
+      type: 'string'
+    },
+    {
+      name: 'profileImage',
+      title: 'Profile Image',
+      type: 'string'
+    }
+  ]
+}
+
+export default userSchema
